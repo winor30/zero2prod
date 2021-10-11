864609df0584c427359b05b539e36848f6ff9d97
Takuma Katsumata <tk.30.r.h@gmail.com>
2021-10-12 00:01:38 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    println!("Hello, world! {}", sum(1, 2));
+    println!("Hello, world! {}", sum(1, 3));
 }
 
 fn sum(p1: u32, p2: u32) -> u32 {
-    p1 + p2 + 1
+    p1 + p2
 }
 
 #[cfg(test)]
