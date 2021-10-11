69079f84361cb928641ecbde47a2f3322b34c06a
Takuma Katsumata <tk.30.r.h@gmail.com>
2021-10-11 23:57:22 +0900

    feat: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {}", sum(1, 2));
+}
+
+fn sum(p1: u32, p2: u32) -> u32 {
+    p1 + p2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn sum_test() {
+        assert_eq!(sum(10, 20), 30);
+    }
 }
